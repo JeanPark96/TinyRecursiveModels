027b604ac41b412775c20775ad18df2c82379852
vwlin <vwl4ac@virginia.edu>
2026-01-12 19:37:34 -0500

    update unimodal script

--- scripts/test_unimodal.sh
+++ scripts/test_unimodal.sh
@@ -1,6 +1,7 @@
 # 1: gpu
 # 2: n_horizon
 # 3: max_obstacles
-# 4: run_name
+# 4: lr
+# 5: run_name
 
-python train_unimodal_faithful.py --run_name $4 --halt_max_steps 16 --gpu_id $1 --horizon_sec $2 --max_obstacles $3 --epochs 10000 --lr 1e-5 --lr_schedule --config_batch_size 32
+python train_unimodal_faithful.py --run_name $5 --halt_max_steps 16 --gpu_id $1 --horizon_sec $2 --max_obstacles $3 --epochs 10000 --lr $4 --lr_schedule --config_batch_size 32
